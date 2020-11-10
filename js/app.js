075a3d7480ab5aec38c812e2109f1e0c80103d42
olehbilykh <74217366+olehbilykh@users.noreply.github.com>
2020-11-10 01:09:26 +0100

    My first commit
    Bla bla

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        sectionsColor: ['#f1f2f6', '#f1f2f6', '#f1f2f6', '#f1f2f6', '#f1f2f6'],
+        anchors: ['home-sec', 'about-sec', 'service-sec', 'project-sec', 'testimony-sec', 'contact-sec'],
+        menu: '#myMenu',
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        touchSensitivity: 5
+    });
+
+    // filterizr
+
+    var filterizd = $('.filter-container').filterizr({
+        animationDuration: .5
+    });
+
+    // Slick slider
+    $('.slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 3,
+        speed: 1000,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 400,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        }]
+    });
+
+    // mobile menu
+    $('.burger').on('click', function () {
+        $('.overlay').toggleClass('mob-active');
+    })
+
+});
